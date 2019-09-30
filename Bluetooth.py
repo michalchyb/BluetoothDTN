a5012a19b3b7c48ee13c7ec9c4bef5f9bb740ef9
Michał Chyb <michalchyb1@gmail.com>
2019-09-30 19:53:57 +0200

    uncommented

--- Bluetooth.py
+++ Bluetooth.py
@@ -5,52 +5,52 @@ from bluetooth import *
 from Helpers import *
 from Messages import *
 
-# lookForAllNearbyDevices()
-#
-# service_matches = find_service(name=b'OBEX Object Push')
-# if len(service_matches) == 0:
-#     print Messages.canNotFindService
-#     sys.exit(0)
-# else:
-#     print Messages.serviceIsFound
-#
-# for x in range(len(service_matches)):
-#     port = service_matches[x]["port"]
-#     name = service_matches[x]["name"]
-#     host = service_matches[x]["host"]
-#     print"--------------------------------------"
-#     print (service_matches[x])
-#     print "name: " + name
-#     print "port: " + str(port)
-#     print "host: " + host
-#     print"--------------------------------------"
-#
-# first_match = service_matches[0]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-# print port
-# print name
-# print host
-# print("Connecting to \"%s\" on %s" % (name, host))
-# client = Client(host, port)
-# client.connect()
-# client.put("test.txt", "Hello world\n")
-# client.disconnect()
-#
-# first_match = service_matches[1]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-# print port
-# print name
-# print host
-# print("Connecting to \"%s\" on %s" % (name, host))
-# client = Client(host, port)
-# client.connect()
-# client.put("test.txt", "Hello world\n")
-# client.disconnect()
-# print "End of transmission"
+lookForAllNearbyDevices()
+
+service_matches = find_service(name=b'OBEX Object Push')
+if len(service_matches) == 0:
+    print Messages.canNotFindService
+    sys.exit(0)
+else:
+    print Messages.serviceIsFound
+
+for x in range(len(service_matches)):
+    port = service_matches[x]["port"]
+    name = service_matches[x]["name"]
+    host = service_matches[x]["host"]
+    print"--------------------------------------"
+    print (service_matches[x])
+    print "name: " + name
+    print "port: " + str(port)
+    print "host: " + host
+    print"--------------------------------------"
+
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+print port
+print name
+print host
+print("Connecting to \"%s\" on %s" % (name, host))
+client = Client(host, port)
+client.connect()
+client.put("test.txt", "Hello world\n")
+client.disconnect()
+
+first_match = service_matches[1]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+print port
+print name
+print host
+print("Connecting to \"%s\" on %s" % (name, host))
+client = Client(host, port)
+client.connect()
+client.put("test.txt", "Hello world\n")
+client.disconnect()
+print "End of transmission"
 
 replace = str(cmdLine("sudo btmgmt find |grep rssi |sort -n |uniq -w 33"))\
     .replace("\n", "")\
@@ -63,4 +63,6 @@ replace = str(cmdLine("sudo btmgmt find |grep rssi |sort -n |uniq -w 33"))\
     .replace("LE Random", "")\
     .replace("LE Public", "")\
     .replace("0x0004", "")
+
+
 print replace
