c6efe35c377599a1b01b0085fcbe9dd90beb1527
Michał Chyb <michalchyb1@gmail.com>
2019-10-08 19:42:04 +0200

    refactor

--- Messages.py
+++ Messages.py
@@ -5,3 +5,4 @@ class Messages:
 
     can_not_find_service = "nie moge znaleźć "
     service_is_found = "serwis znaleziony"
+    linux_command_for_searching_devices = "sudo btmgmt find |grep rssi |sort -n |uniq -w 33"
