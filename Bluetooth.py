ef226c33010bb2e032b33b02dae4076f14733202
Michał Chyb <michalchyb1@gmail.com>
2019-10-10 19:58:03 +0200

    deleted death code

--- Bluetooth.py
+++ Bluetooth.py
@@ -2,52 +2,6 @@
 import time
 from Helpers import *
 from Messages import *
-import threading
-
-#
-# for x in range(len(service_matches)):
-#     port = service_matches[x]["port"]
-#     name = service_matches[x]["name"]
-#     host = service_matches[x]["host"]
-#     print"--------------------------------------"
-#     print (service_matches[x])
-#     print "name: " + name
-#     print "port: " + str(port)
-#     print "host: " + host
-#     print"--------------------------------------"
-#
-# first_match = service_matches[0]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-# print port
-# print name
-# print host
-# print("Connecting to \"%s\" on %s" % (name, host))
-# client = Client(host, port)
-# client.connect()
-# client.put("test.txt", "Hello world\n")
-# client.disconnect()
-#
-# first_match = service_matches[1]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-# print port
-# print name
-# print host
-# print("Connecting to \"%s\" on %s" % (name, host))
-# client = Client(host, port)
-# client.connect()
-# client.put("test.txt", "Hello world\n")
-# client.disconnect()
-# print "End of transmission"
-
-# look_for_all_nearby_devices()
-#
-# find_obex_services_devices()
-
-list_of_trusted_devices = ["18:F0:E4:3A:5A:31"]
 
 # dictionary_MAC_and_rssi = create_MAC_and_RSSI_dictionary(cmd_line("sudo btmgmt find |grep rssi |sort -n |uniq -w 33"))
 
@@ -55,6 +9,7 @@ list_of_trusted_devices = ["18:F0:E4:3A:5A:31"]
 # dictionary_MAC_and_rssi = create_MAC_and_RSSI_dictionary(string)
 
 # filtering(list_of_trusted_devices, dictionary_MAC_and_rssi)
+list_of_trusted_devices = ["18:F0:E4:3A:5A:31"]
 
 timeout = 60  # [seconds]
 
