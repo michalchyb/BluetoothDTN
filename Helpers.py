7e53518b0406b8f3981fabf045f972dfc36f5217
Michał Chyb <michalchyb1@gmail.com>
2019-10-27 11:49:15 +0100

    changed way to send file

--- Helpers.py
+++ Helpers.py
@@ -58,12 +58,15 @@ def create_MAC_and_RSSI_dictionary(s):
 
 
 def sendFile(name, host, port):
-    print "will send a file"
-    print("Connecting to \"%s\" on %s" % (name, host))
-    client = Client(host, port)
-    client.connect()
-    client.put("test.txt", "Hello world\n")
-    client.disconnect()
+    server_socket = BluetoothSocket(RFCOMM)
+
+    server_socket.bind(("", 3))
+    server_socket.listen(1)
+    client_socket, address = server_socket.accept()
+    data = client_socket.recv(1024)
+    print "received [%s]" % data
+    client_socket.close()
+    server_socket.close()
 
 
 def get_host_data(device):
