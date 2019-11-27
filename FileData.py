31cbedb0a8a131d706f66ca3e0d6b00674b21451
Michał Chyb <michalchyb1@gmail.com>
2019-11-27 14:43:16 +0100

    added write to file method for a data comes from device to device

--- FileData.py
+++ FileData.py
@@ -1,4 +1,9 @@
 def read_file():
-    with open('file.txt', 'r') as file:
+    with open('fileToSend.txt', 'r') as file:
         data = file.read().replace('\n', '')
         return data
+
+
+def write_file(data):
+    with open("receivedFile.txt", "w") as text_file:
+        text_file.write(data)
