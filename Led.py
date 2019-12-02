3fa0d3abdd35d79133dfc7e3b0bc40f7faf50d6c
Michał Chyb <michalchyb1@gmail.com>
2019-12-02 11:00:34 +0100

    changed blinking_leds() to control only power led

--- Led.py
+++ Led.py
@@ -4,12 +4,11 @@ import time
 
 def blinking_leds():
     os.system("echo gpio | sudo tee /sys/class/leds/led1/trigger")
-    os.system("echo gpio | sudo tee /sys/class/leds/led0/trigger")
     time.sleep(1)
     os.system("echo input | sudo tee /sys/class/leds/led1/trigger")
-    os.system("echo input | sudo tee /sys/class/leds/led0/trigger")
     time.sleep(1)
-    os.system("echo cpu0 | sudo tee /sys/class/leds/led0/trigger")
+    os.system("echo gpio | sudo tee /sys/class/leds/led1/trigger")
+    time.sleep(1)
     os.system("echo cpu0 | sudo tee /sys/class/leds/led1/trigger")
 
 
