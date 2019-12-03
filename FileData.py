6d3cd419df01506e8b19da51e1e38e6718d45d09
Michał Chyb <michalchyb1@gmail.com>
2019-12-03 14:34:09 +0100

    added file methods

--- FileData.py
+++ FileData.py
@@ -1,5 +1,8 @@
 import os
 from zipfile import ZipFile
+import shutil
+
+from Messages import *
 
 
 def read_file():
@@ -26,18 +29,50 @@ def write_zip_file(data):
     f.close()
 
 
-def file_list():
-    path = '/home/michalch/PycharmProjects'
+def get_current_path():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def get_file_list():
     files = []
-    for r, d, f in os.walk(path):
+    for r, d, f in os.walk(get_current_path()):
         for file in f:
             if '.zip' in file:
                 files.append(os.path.join(r, file))
-
-    for f in files:
-        print(f)
+    return files
 
 
 def unzip_file():
-    with ZipFile('/home/michalch/PycharmProjects/tests/file.zip', 'r') as zipObj:
+    with ZipFile(get_current_path() + '/file.zip', 'r') as zipObj:
         zipObj.extractall('temp')
+
+
+def create_directory(folder_name):
+    path = get_current_path() + Messages.slash + folder_name
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Directory %s already existed or creation of the directory %s failed" % (path, path))
+    else:
+        print ("Successfully created the directory %s " % path)
+
+
+def remove_directory(folder_name):
+    os.rmdir(folder_name)
+
+
+def check_file_zip(file_name):
+    file_list = get_file_list()
+    if get_current_path() + Messages.slash + file_name in file_list:
+        return True
+
+
+def prepare_directory():
+    create_directory("temp")
+    create_directory("final data")
+
+
+def move_file_to_directory(destination_folder_name, file_to_move):
+    source_file_path = get_current_path() + Messages.slash + file_to_move
+    destination_file_path = get_current_path() + Messages.slash + destination_folder_name + Messages.slash + file_to_move
+    shutil.move(source_file_path, destination_file_path)
